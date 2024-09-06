445087d804f416b59eba553a58fe02748c67245b
Eren0414 <kritof.jager.2006@gmail.com>
2024-09-06 11:13:33 +0200

    homepage 1/2

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -12,30 +12,12 @@
         <div class="links">
             <ul class="sec1">
                 <li><a href="">Home</a></li>
-                <li><a href="">Tours</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">Contact</a></li>
                 <li><a href="">About</a></li>
             </ul>
         </div>
      </div>
     </header>
-
-    <section class="section1"> 
-        <div class="subsec">
-        <div>
-            <p class="heading">H-H Travels</p>
-        </div>
-        <div>
-            <p class="subheading">Travel with Us</p>
-        </div>
-        <div>
-            <button class="butt">
-                Explore Packages
-            </button>
-        </div></div>
-    </section>
-    </body>
+</body>
 </template>
 
 <script>
