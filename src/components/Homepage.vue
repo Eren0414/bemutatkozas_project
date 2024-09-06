44da4edc1f732994b79679802f4e16251b1587e1
Eren0414 <kritof.jager.2006@gmail.com>
2024-09-06 11:22:42 +0200

    page kesz

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,23 +1,5 @@
 <template>
-<head>
-    <title>Hello, world!</title>
-</head>
-    <body>
-    <header>
-     <div class="container">
-        <div class="logo">
-           <p class="main">H-H Travels</p>
-           <p class="sub">Travel with Us</p>
-        </div>
-        <div class="links">
-            <ul class="sec1">
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-            </ul>
-        </div>
-     </div>
-    </header>
-</body>
+
 </template>
 
 <script>
