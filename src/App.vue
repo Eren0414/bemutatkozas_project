5677e7d8396261a0759356336d61133254e9db72
Eren0414 <kritof.jager.2006@gmail.com>
2024-09-08 19:15:36 +0200

    resp kesz

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ import Kristofv from './views/Kristofv.vue';
 
 <template>
   <header>
+    <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello, world!</title>
   <body>
     <header>
